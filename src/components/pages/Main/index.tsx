b6c305e09a9b6853298d91646e45775b9bc14532
jinsunee <jsunee.dev@gmail.com>
2023-11-23 14:21:05 +0900

    scss로 변경

--- src/components/pages/Main/index.tsx
+++ src/components/pages/Main/index.tsx
@@ -6,12 +6,9 @@ import { FiCheck, FiChevronLeft, FiChevronRight, FiPlus } from "react-icons/fi";
 import { InputMode, TodoItem } from "../../../types";
 import { Spacing } from "../../shared/Spacing";
 import useTodoItems from "./hooks/useTodoItems";
+import "./index.scss";
 
 const defaultInputMode: InputMode = { type: "default" };
-
-// hook -> ui함수를 Return하지 않고, state와 관련한 로직을 실행하고 내보내는 곳.
-// use+아무거나
-// useCurrentDate
 
 export default function Main() {
   const [currentDate, setCurrentDate] = useState(new Date());
@@ -56,59 +53,28 @@ export default function Main() {
   }
 
   return (
-    <div style={{ position: "relative", height: "100vh", overflow: "hidden" }}>
-      <div
-        style={{
-          position: "sticky",
-          top: 0,
-          left: 0,
-          right: 0,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "20px 0",
-        }}
-      >
-        <div onClick={handleMovePreviousDate} style={{ padding: 10 }}>
+    <div className="container">
+      <div className="date-navigator">
+        <div onClick={handleMovePreviousDate} className="move-button">
           <FiChevronLeft size={25} />
         </div>
-        <div style={{ textAlign: "center" }}>
-          <div style={{ fontSize: 24, fontWeight: "bold" }}>
+        <div className="text-center">
+          <div className="date-big-font">
             {format(currentDate, "MM월 dd일")}
           </div>
-          <div style={{ color: "#BCBCBC" }}>
+          <div className="date-normal-font">
             {format(currentDate, "yyyy년")}
           </div>
         </div>
-        <div onClick={handleMoveNextDate} style={{ padding: 10 }}>
+        <div onClick={handleMoveNextDate} className="move-button">
           <FiChevronRight size={25} />
         </div>
       </div>
-      <div
-        style={{
-          padding: 20,
-          height: "100%",
-          boxSizing: "border-box",
-          overflowY: "auto",
-          display: "flex",
-          flexDirection: "column",
-          gap: 14,
-        }}
-      >
+      <div className="todo-list">
         {/* AddInput */}
         {inputMode.type === "add" && (
           <div>
             <input
-              style={{
-                background: "none",
-                border: "none",
-                borderBottom: "1px solid #666666",
-                outline: "none",
-                color: "#fff",
-                padding: "6px 0",
-                fontSize: "1em",
-                width: "100%",
-              }}
               type="text"
               placeholder="새로운 할일을 입력할 수 있어요 :)"
               value={addInputValue}
@@ -116,30 +82,10 @@ export default function Main() {
             />
             <Spacing size={5} />
             <div>
-              <button
-                onClick={handleResetInputMode}
-                style={{
-                  background: "none",
-                  border: "1px solid #CFFF48",
-                  borderRadius: 14,
-                  color: "#CFFF48",
-                  padding: "5px 10px",
-                  marginRight: 4,
-                  fontWeight: 700,
-                }}
-              >
+              <button onClick={handleResetInputMode} className="cancel-button">
                 취소
               </button>
-              <button
-                onClick={onAddTodoItem}
-                style={{
-                  background: "#CFFF48",
-                  padding: "5px 10px",
-                  border: "1px solid transparent",
-                  borderRadius: 14,
-                  fontWeight: 700,
-                }}
-              >
+              <button onClick={onAddTodoItem} className="save-button">
                 저장
               </button>
             </div>
@@ -155,16 +101,6 @@ export default function Main() {
               {isEditMode && (
                 <div>
                   <input
-                    style={{
-                      background: "none",
-                      border: "none",
-                      borderBottom: "1px solid #666666",
-                      outline: "none",
-                      color: "#fff",
-                      padding: "6px 0",
-                      fontSize: "1em",
-                      width: "100%",
-                    }}
                     type="text"
                     placeholder="새로운 할일을 입력할 수 있어요 :)"
                     value={editInputValue}
@@ -174,42 +110,18 @@ export default function Main() {
                   <div>
                     <button
                       onClick={handleResetInputMode}
-                      style={{
-                        background: "none",
-                        border: "1px solid #CFFF48",
-                        borderRadius: 14,
-                        color: "#CFFF48",
-                        padding: "5px 10px",
-                        marginRight: 4,
-                        fontWeight: 700,
-                      }}
+                      className="cancel-button"
                     >
                       취소
                     </button>
-                    <button
-                      onClick={onEditTodoItem}
-                      style={{
-                        background: "#CFFF48",
-                        padding: "5px 10px",
-                        border: "1px solid transparent",
-                        borderRadius: 14,
-                        fontWeight: 700,
-                      }}
-                    >
+                    <button onClick={onEditTodoItem} className="save-button">
                       저장
                     </button>
                   </div>
                 </div>
               )}
               {!isEditMode && !item.isDone && (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                  onClick={() => handleEditMode(item)}
-                >
+                <div className="todo-item" onClick={() => handleEditMode(item)}>
                   <div>{item.content}</div>
                   <div
                     onClick={(e) => {
@@ -222,21 +134,8 @@ export default function Main() {
                 </div>
               )}
               {!isEditMode && item.isDone && (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <div
-                    style={{
-                      textDecoration: "line-through",
-                      color: "#666666",
-                    }}
-                  >
-                    {item.content}
-                  </div>
+                <div className="todo-item">
+                  <div className="done">{item.content}</div>
                   <div onClick={() => onCancelDone(index)}>
                     <FiCheck color="#CFFF48" size={26} />
                   </div>
@@ -246,25 +145,7 @@ export default function Main() {
           );
         })}
       </div>
-      <div
-        onClick={handleAddMode}
-        style={{
-          position: "absolute",
-          right: 20,
-          bottom: 20,
-          width: 32,
-          height: 32,
-          borderRadius: 30,
-          background: "#CFFF48",
-          // text-align vs flex center
-          // text-align: 인라인 요소, text를 정렬하는 속성
-          // <div style={{ textAlign: 'center' }}> <div>텍스트</div> </div>
-          // <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}> <div>텍스트</div> </div>
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <div onClick={handleAddMode} className="add-button">
         <FiPlus color="#1E1E1E" size={24} />
       </div>
     </div>
