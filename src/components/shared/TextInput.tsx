133462838248a3a5ce763ef73bbb5b353369c094
jinsunee <jsunee.dev@gmail.com>
2023-12-05 17:15:40 +0900

    ui: 자잘한 수정

--- src/components/shared/TextInput.tsx
+++ src/components/shared/TextInput.tsx
@@ -6,7 +6,7 @@ export const TextInput = styled.input`
   border: none;
   border-bottom: 1px solid ${colors.gray[1]};
   outline: none;
-  color: var(--color-white);
+  color: ${colors.white};
   padding: 6px 0;
   font-size: 1em;
   width: 100%;
